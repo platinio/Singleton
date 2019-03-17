ee26eb39cc3682b1e5e725ed540dfff2828e14b1
Platinio <platinio94@gmail.com>
2019-03-17 11:24:17 -0300

    remove is alive

--- Assets/Singleton.cs
+++ Assets/Singleton.cs
@@ -1,103 +1,113 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Platinio
 {
 
-	/// <summary>
-	/// Basic singleton class
-	/// </summary>
-	public class Singleton<T> : MonoBehaviour where T : Singleton<T>
-	{
-		private static T m_instance = null;
 
-		public static T instance
-		{
-			get
-			{
-				if(m_instance == null)
-				{
-					//get all the singletones
-					T[] singletons = GameObject.FindObjectsOfType( typeof(T) ) as T[];
+    /// <summary>
+    /// Basic singleton class
+    /// </summary>
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        private static T m_instance = null;
 
-					if(singletons != null)
-					{
-						if(singletons.Length == 1)
-						{
-
-							m_instance = singletons[0];
-							return m_instance;
-						}
-
-						else if(singletons.Length > 1)
-						{
-							Debug.LogWarning("You have more thah one " + typeof( T ).Name + " In the scene, you only need one , all the instances will be destroyed for create a new one");
-
-							m_instance = singletons[0];
-
-							for(int n = 1 ; n < singletons.Length ; n++)
-							{
-								Destroy( singletons[n].gameObject );
-							}
-
-							return m_instance;
-						}
-					}
-
-					GameObject go = new GameObject( typeof(T).Name );
-					m_instance = go.AddComponent<T>();
-
-				}
-
-				return m_instance;
-			}
-		}
+        public static T instance
+        {
+            get
+            {
 
 
+                if (m_instance == null)
+                {
 
-		public static bool IsAlive
-		{
-			get
-			{
-				if(m_instance == null)
-					return false;
-				return m_instance.m_alive;
-			}
-		}
+                    //get all the singletones
+                    T[] singletons = GameObject.FindObjectsOfType(typeof(T)) as T[];
 
-		public static bool ShouldDestroyOnLoad
-		{
-			get
-			{
-				return m_instance.m_shouldDestroyOnLoad;
-			}
-		}
+                    if (singletons != null)
+                    {
+                        if (singletons.Length == 1)
+                        {
+
+                            m_instance = singletons[0];
+                            return m_instance;
+                        }
+
+                        else if (singletons.Length > 1)
+                        {
+                            Debug.LogWarning("You have more thah one " + typeof(T).Name + " In the scene, you only need one , all the instances will be destroyed for create a new one");
+
+                            m_instance = singletons[0];
+
+                            for (int n = 1; n < singletons.Length; n++)
+                            {
+                                Destroy(singletons[n].gameObject);
+                            }
+
+                            return m_instance;
+                        }
+                    }
+
+                    //if the application is quitting dont create more singletons
+                    if (ApplicationIsQuitting)
+                        return null;
+
+                    GameObject go = new GameObject(typeof(T).Name);
+                    m_instance = go.AddComponent<T>();
+
+                }
+
+                return m_instance;
+
+            }
+        }
 
 
-		[SerializeField] private bool 	m_shouldDestroyOnLoad	= true;
-		private bool m_alive = true;
 
-		protected virtual void Start()
-		{
-			T _instance = instance;
-
-			if(!ShouldDestroyOnLoad)
-			{
-				DontDestroyOnLoad( m_instance );
-			}
-		}
-
-		protected virtual void OnDestroy()
-		{
-			m_alive = false;
-		}
-
-		protected virtual void OnApplicationQuit()
-		{
-			m_alive = false;
-		}
+        public static bool ApplicationIsQuitting = false;
 
 
-	}
+        public static bool DestroyOnLoad
+        {
+            get
+            {
+                return m_instance.m_destroyOnLoad;
+            }
+        }
+
+
+        [SerializeField] private bool m_destroyOnLoad = true;
+
+        protected virtual void Awake()
+        {
+            if (!ReferenceEquals((object)instance, (object)gameObject.GetComponent<T>()))
+            {
+                //destroy repeat instance
+                Destroy(gameObject);
+            }
+        }
+
+        protected virtual void Start()
+        {
+            //get the instance
+            T singleton = instance;
+
+            if (!DestroyOnLoad)
+            {
+                DontDestroyOnLoad(m_instance);
+            }
+
+
+
+        }
+
+
+        protected virtual void OnApplicationQuit()
+        {
+            ApplicationIsQuitting = true;
+
+        }
+
+
+    }
 
 }
